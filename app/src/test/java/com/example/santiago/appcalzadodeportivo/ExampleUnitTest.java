903157531cbc9ed6e805955914b180c1bb098827
Walter Santiago <android@cuc.edu.co>
2017-10-03 20:44:59 -0500

    AppCalzadoDeportivo versión 1.0 final con Test (Pruebas unitarias)

--- app/src/test/java/com/example/santiago/appcalzadodeportivo/ExampleUnitTest.java
+++ app/src/test/java/com/example/santiago/appcalzadodeportivo/ExampleUnitTest.java
@@ -11,7 +11,218 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void combinacion1(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 0;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 120000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion2(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 1;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 140000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion3(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 2;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 130000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion4(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 0;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 50000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion5(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 1;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 80000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion6(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 0;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 2;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 100000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion7(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 0;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 100000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion8(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 1;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 130000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion9(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 0;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 2;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 110000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion10(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 0;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 60000;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion11(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 1;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 70000;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
+    }
+
+    @Test
+    public void combinacion12(){
+
+        //sexo 0 = Masculino, sexo 1 = Femenino
+        int sexo = 1;
+        //tipoDeZapato 0 = Zapatilla, tipoDeZapato 1 = Clasico
+        int tipoDeZapato = 1;
+        //MarcaDeZapatoEscogida 0 = Nike, MarcaDeZapatoEscogida 1 = Adidas, MarcaDeZapatoEscogida 2 = Puma
+        int marca = 2;
+        //Cantidad cualquier numero
+        int cantidad = 1;
+
+        double ve = 120000.0;
+        double vr = Metodos.comprar(sexo,tipoDeZapato,marca,cantidad);
+        assertEquals(ve,vr,0);
+
     }
 }
